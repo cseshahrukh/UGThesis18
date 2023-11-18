8c8093f77e1b1db6b60d767c8ad2f39048138f98
cse <cseshahrukh@gmail.com>
2023-11-18 23:03:22 +0600

    first commit

---
+++ MOON-main/test.py
@@ -0,0 +1,5 @@
+try:
+    import numpy as np
+    print("NumPy is installed and imported successfully.")
+except ImportError:
+    print("NumPy is not installed. Please install it using 'pip install numpy'.")
